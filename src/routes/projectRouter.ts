fa7146baea39822c8a67e15a831d75fddab71202
Orveo <orveo1902@gmail.com>
2026-04-16 22:07:24 -0400

    Se agrego un middleware validador y se validaron los campos del post

--- src/routes/projectRouter.ts
+++ src/routes/projectRouter.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
+import {body} from "express-validator"
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
 
 
 const router = Router()
 
-router.post('/',ProjectController.createProject)
+router.post('/',
+    body('project_name').notEmpty().withMessage('The name of the project is required'),
+    body('client_name').notEmpty().withMessage('The name of the client is required'),
+    body('description').notEmpty().withMessage('Description is required'),
+    handleInputErrors,
+    ProjectController.createProject
+)
 router.get('/',ProjectController.getAllProjects)
 
 export default router
