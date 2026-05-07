171e175bd9ea79d0143550633c5bc61985a39586
Orveo <orveo1902@gmail.com>
2026-05-07 12:45:41 -0400

    Se agrega el usuario a la base de datos y se valida si existe o no

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,7 +1,27 @@
 import type { Request, Response } from "express"
+import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 
 export class AuthController {
     static createAccount = async(req: Request, res: Response) => {
-        res.send('Desde /api/auth')
+        try {
+            const {password, email} = req.body
+            //check if exist email. 
+            const userExist = await User.findOne({email})
+            
+            if(userExist) {
+                const error = new Error('The user is already exist')
+                return res.status(409).json({error: error.message})
+            }
+
+            //Crear un usuario nuevo
+            const user = new User(req.body)
+            //Hasheando el password del usuario. 
+            user.password = await hashPassword(password)
+            await user.save()
+            res.send('User created succesfully, check your email for verify your user.')
+        } catch (error) {
+            res.status(500).json({error: 'There was an error'})
+        }
     }
 }
