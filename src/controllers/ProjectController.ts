050d5f57533f9491879c4024c7d374057708c757
Orveo <orveo1902@gmail.com>
2026-05-21 11:07:28 -0400

    Los colaboradores pueden ver en su perfil los proyectos que pertenecen

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -19,7 +19,8 @@ export class ProjectController {
         try {
             const projects = await Project.find({
                 $or: [ //Con esta condición se le dice que traiga nada más los proyectos en el cual el manager creó. 
-                    {manager: {$in: req.user._id}}
+                    {manager: {$in: [req.user._id]}},
+                    {team: {$in: [req.user._id]}}
                 ]
             })
             res.json(projects)
@@ -38,7 +39,7 @@ export class ProjectController {
                 return
             }
 
-            if(project.manager.toString() !== req.user._id.toString()){
+            if(project.manager.toString() !== req.user._id.toString() && !project.team.includes(req.user._id )){
                 res.status(404).json({ error: 'Action no validate'})
                 return
             }
